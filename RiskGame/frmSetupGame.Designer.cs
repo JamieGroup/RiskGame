3a6b84a923fb9d74908260b8d4da352c24e40fc0
C2k-DEV <enniqa@gmail.com>
2022-11-22 13:35:27 +0000

    Added colour acceptability range.

--- RiskGame/frmSetupGame.Designer.cs
+++ RiskGame/frmSetupGame.Designer.cs
@@ -39,26 +39,31 @@ namespace RiskGame
             this.lbSelect1 = new System.Windows.Forms.Label();
             this.lbSelect2 = new System.Windows.Forms.Label();
             this.pnlAI1Settings = new System.Windows.Forms.Panel();
-            this.pnlAI2Settings = new System.Windows.Forms.Panel();
             this.btnAI1Colour = new System.Windows.Forms.Button();
+            this.pnlAI2Settings = new System.Windows.Forms.Panel();
             this.btnAI2Colour = new System.Windows.Forms.Button();
             this.btnPlayerColour = new System.Windows.Forms.Button();
             this.pbStartMultiplayer = new System.Windows.Forms.PictureBox();
             this.pbStartSingleplayer = new System.Windows.Forms.PictureBox();
-            this.pbDice = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlDice = new System.Windows.Forms.Panel();
+            this.pnlSecretMode = new System.Windows.Forms.Panel();
+            this.pbSecretMode = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.trbrAISelector)).BeginInit();
             this.pnlAI1Settings.SuspendLayout();
             this.pnlAI2Settings.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbStartMultiplayer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbStartSingleplayer)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pbDice)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.pnlSecretMode.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSecretMode)).BeginInit();
             this.SuspendLayout();
             // 
             // lbPlayerColour
             // 
             this.lbPlayerColour.AutoSize = true;
             this.lbPlayerColour.Font = new System.Drawing.Font("Segoe UI Semibold", 20F, System.Drawing.FontStyle.Bold);
-            this.lbPlayerColour.Location = new System.Drawing.Point(12, 120);
+            this.lbPlayerColour.Location = new System.Drawing.Point(12, 58);
             this.lbPlayerColour.Name = "lbPlayerColour";
             this.lbPlayerColour.Size = new System.Drawing.Size(170, 37);
             this.lbPlayerColour.TabIndex = 0;
@@ -98,7 +103,7 @@ namespace RiskGame
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 59);
+            this.label2.Location = new System.Drawing.Point(3, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(409, 30);
             this.label2.TabIndex = 4;
@@ -106,10 +111,10 @@ namespace RiskGame
             // 
             // trbrAISelector
             // 
-            this.trbrAISelector.Location = new System.Drawing.Point(447, 60);
+            this.trbrAISelector.Location = new System.Drawing.Point(8, 52);
             this.trbrAISelector.Maximum = 2;
             this.trbrAISelector.Name = "trbrAISelector";
-            this.trbrAISelector.Size = new System.Drawing.Size(328, 45);
+            this.trbrAISelector.Size = new System.Drawing.Size(496, 45);
             this.trbrAISelector.TabIndex = 5;
             this.trbrAISelector.Value = 1;
             this.trbrAISelector.Scroll += new System.EventHandler(this.trbrAISelector_Scroll);
@@ -118,7 +123,7 @@ namespace RiskGame
             // 
             this.lbSelect0.AutoSize = true;
             this.lbSelect0.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbSelect0.Location = new System.Drawing.Point(450, 84);
+            this.lbSelect0.Location = new System.Drawing.Point(11, 80);
             this.lbSelect0.Name = "lbSelect0";
             this.lbSelect0.Size = new System.Drawing.Size(24, 30);
             this.lbSelect0.TabIndex = 6;
@@ -128,7 +133,7 @@ namespace RiskGame
             // 
             this.lbSelect1.AutoSize = true;
             this.lbSelect1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbSelect1.Location = new System.Drawing.Point(601, 84);
+            this.lbSelect1.Location = new System.Drawing.Point(245, 80);
             this.lbSelect1.Name = "lbSelect1";
             this.lbSelect1.Size = new System.Drawing.Size(24, 30);
             this.lbSelect1.TabIndex = 7;
@@ -138,7 +143,7 @@ namespace RiskGame
             // 
             this.lbSelect2.AutoSize = true;
             this.lbSelect2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbSelect2.Location = new System.Drawing.Point(751, 84);
+            this.lbSelect2.Location = new System.Drawing.Point(480, 80);
             this.lbSelect2.Name = "lbSelect2";
             this.lbSelect2.Size = new System.Drawing.Size(24, 30);
             this.lbSelect2.TabIndex = 8;
@@ -148,19 +153,10 @@ namespace RiskGame
             // 
             this.pnlAI1Settings.Controls.Add(this.lbAI1Colour);
             this.pnlAI1Settings.Controls.Add(this.btnAI1Colour);
-            this.pnlAI1Settings.Location = new System.Drawing.Point(19, 172);
+            this.pnlAI1Settings.Location = new System.Drawing.Point(22, 266);
             this.pnlAI1Settings.Name = "pnlAI1Settings";
             this.pnlAI1Settings.Size = new System.Drawing.Size(695, 56);
             this.pnlAI1Settings.TabIndex = 9;
-            // 
-            // pnlAI2Settings
-            // 
-            this.pnlAI2Settings.Controls.Add(this.lbAI2Colour);
-            this.pnlAI2Settings.Controls.Add(this.btnAI2Colour);
-            this.pnlAI2Settings.Location = new System.Drawing.Point(19, 221);
-            this.pnlAI2Settings.Name = "pnlAI2Settings";
-            this.pnlAI2Settings.Size = new System.Drawing.Size(695, 56);
-            this.pnlAI2Settings.TabIndex = 10;
             // 
             // btnAI1Colour
             // 
@@ -169,6 +165,15 @@ namespace RiskGame
             this.btnAI1Colour.Size = new System.Drawing.Size(179, 46);
             this.btnAI1Colour.TabIndex = 11;
             this.btnAI1Colour.UseVisualStyleBackColor = true;
+            // 
+            // pnlAI2Settings
+            // 
+            this.pnlAI2Settings.Controls.Add(this.lbAI2Colour);
+            this.pnlAI2Settings.Controls.Add(this.btnAI2Colour);
+            this.pnlAI2Settings.Location = new System.Drawing.Point(22, 315);
+            this.pnlAI2Settings.Name = "pnlAI2Settings";
+            this.pnlAI2Settings.Size = new System.Drawing.Size(695, 56);
+            this.pnlAI2Settings.TabIndex = 10;
             // 
             // btnAI2Colour
             // 
@@ -180,7 +185,7 @@ namespace RiskGame
             // 
             // btnPlayerColour
             // 
-            this.btnPlayerColour.Location = new System.Drawing.Point(208, 120);
+            this.btnPlayerColour.Location = new System.Drawing.Point(208, 58);
             this.btnPlayerColour.Name = "btnPlayerColour";
             this.btnPlayerColour.Size = new System.Drawing.Size(179, 46);
             this.btnPlayerColour.TabIndex = 13;
@@ -190,7 +195,7 @@ namespace RiskGame
             // pbStartMultiplayer
             // 
             this.pbStartMultiplayer.Image = global::RiskGame.Properties.Resources.StartMultiPlayer;
-            this.pbStartMultiplayer.Location = new System.Drawing.Point(514, 338);
+            this.pbStartMultiplayer.Location = new System.Drawing.Point(514, 391);
             this.pbStartMultiplayer.Name = "pbStartMultiplayer";
             this.pbStartMultiplayer.Size = new System.Drawing.Size(261, 100);
             this.pbStartMultiplayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -201,7 +206,7 @@ namespace RiskGame
             // pbStartSingleplayer
             // 
             this.pbStartSingleplayer.Image = global::RiskGame.Properties.Resources.StartSinglePlayer;
-            this.pbStartSingleplayer.Location = new System.Drawing.Point(22, 338);
+            this.pbStartSingleplayer.Location = new System.Drawing.Point(22, 391);
             this.pbStartSingleplayer.Name = "pbStartSingleplayer";
             this.pbStartSingleplayer.Size = new System.Drawing.Size(261, 100);
             this.pbStartSingleplayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -209,33 +214,60 @@ namespace RiskGame
             this.pbStartSingleplayer.TabStop = false;
             this.pbStartSingleplayer.Click += new System.EventHandler(this.pbStartSingleplayer_Click);
             // 
-            // pbDice
+            // panel1
             // 
-            this.pbDice.Image = global::RiskGame.Properties.Resources.dice;
-            this.pbDice.Location = new System.Drawing.Point(714, 120);
-            this.pbDice.Name = "pbDice";
-            this.pbDice.Size = new System.Drawing.Size(61, 46);
-            this.pbDice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbDice.TabIndex = 14;
-            this.pbDice.TabStop = false;
-            this.pbDice.Click += new System.EventHandler(this.pbDice_Click);
+            this.panel1.Controls.Add(this.pnlDice);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lbSelect0);
+            this.panel1.Controls.Add(this.lbSelect1);
+            this.panel1.Controls.Add(this.lbSelect2);
+            this.panel1.Controls.Add(this.trbrAISelector);
+            this.panel1.Location = new System.Drawing.Point(22, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(525, 114);
+            this.panel1.TabIndex = 17;
+            // 
+            // pnlDice
+            // 
+            this.pnlDice.BackgroundImage = global::RiskGame.Properties.Resources.dice;
+            this.pnlDice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlDice.Location = new System.Drawing.Point(418, 3);
+            this.pnlDice.Name = "pnlDice";
+            this.pnlDice.Size = new System.Drawing.Size(86, 52);
+            this.pnlDice.TabIndex = 18;
+            this.pnlDice.Click += new System.EventHandler(this.pbDice_Click);
+            // 
+            // pnlSecretMode
+            // 
+            this.pnlSecretMode.Controls.Add(this.pbSecretMode);
+            this.pnlSecretMode.Location = new System.Drawing.Point(479, 12);
+            this.pnlSecretMode.Name = "pnlSecretMode";
+            this.pnlSecretMode.Size = new System.Drawing.Size(200, 100);
+            this.pnlSecretMode.TabIndex = 18;
+            // 
+            // pbSecretMode
+            // 
+            this.pbSecretMode.Image = global::RiskGame.Properties.Resources.Tutorial_Checkbox;
+            this.pbSecretMode.Location = new System.Drawing.Point(3, 7);
+            this.pbSecretMode.Name = "pbSecretMode";
+            this.pbSecretMode.Size = new System.Drawing.Size(91, 85);
+            this.pbSecretMode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSecretMode.TabIndex = 3;
+            this.pbSecretMode.TabStop = false;
+            this.pbSecretMode.Click += new System.EventHandler(this.pbSecretMode_Click);
             // 
             // frmSetupGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.pnlSecretMode);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pbStartMultiplayer);
             this.Controls.Add(this.pbStartSingleplayer);
-            this.Controls.Add(this.pbDice);
             this.Controls.Add(this.btnPlayerColour);
             this.Controls.Add(this.pnlAI2Settings);
             this.Controls.Add(this.pnlAI1Settings);
-            this.Controls.Add(this.lbSelect2);
-            this.Controls.Add(this.lbSelect1);
-            this.Controls.Add(this.lbSelect0);
-            this.Controls.Add(this.trbrAISelector);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbPlayerColour);
             this.Name = "frmSetupGame";
@@ -248,7 +280,10 @@ namespace RiskGame
             this.pnlAI2Settings.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbStartMultiplayer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbStartSingleplayer)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pbDice)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlSecretMode.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSecretMode)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -270,8 +305,11 @@ namespace RiskGame
         private System.Windows.Forms.Button btnAI1Colour;
         private System.Windows.Forms.Button btnAI2Colour;
         private System.Windows.Forms.Button btnPlayerColour;
-        private System.Windows.Forms.PictureBox pbDice;
         private System.Windows.Forms.PictureBox pbStartSingleplayer;
         private System.Windows.Forms.PictureBox pbStartMultiplayer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlDice;
+        private System.Windows.Forms.Panel pnlSecretMode;
+        private System.Windows.Forms.PictureBox pbSecretMode;
     }
 }
