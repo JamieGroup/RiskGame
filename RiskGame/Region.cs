eb248a2dcc5b835f7b167e651a43246632950eb8
C2k-DEV <enniqa@gmail.com>
2022-12-05 12:55:26 +0000

    Added region class

---
+++ RiskGame/Region.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskGame
+{
+    class Region
+    {
+        public int owner { get; set; }
+        public string territory { get; set; }
+        public string name { get; set; }
+
+        public Region(string _name)
+        {
+            name = _name;
+        }
+        
+        public void SetTerritory()
+        {
+            territory = name.Split('_')[0];
+        }
+        public string GetOwner()
+        {
+            return $"Player {owner}";
+        }
+    }
+}
