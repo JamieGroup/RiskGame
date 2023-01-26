84d0db5e2a6d644371a88c275b2d57ba5ff01ccb
C2k-DEV <enniqa@gmail.com>
2023-01-26 12:53:08 +0000

    * Added the serializer

---
+++ RiskGame/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace RiskGame
+{
+    public class Serializer
+    {
+        public static bool SerializePlayer(Plys obj)
+        {
+            try
+            {
+                Stream stream = File.Open("SerializedPlayer.bin", FileMode.Create);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                bFormatter.Serialize(stream, obj);
+                stream.Close();
+                return true;
+            }
+            
+        }
+    }
+}
