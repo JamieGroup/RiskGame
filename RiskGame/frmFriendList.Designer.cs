9cc567c360a3436c10db65d517a30cdc3e0b09c1
C2k-DEV <enniqa@gmail.com>
2023-02-27 12:54:55 +0000

    * Tried adding friends list

--- RiskGame/frmFriendList.Designer.cs
+++ RiskGame/frmFriendList.Designer.cs
@@ -30,6 +30,14 @@ namespace RiskGame
         private void InitializeComponent()
         {
             this.lbTitle = new System.Windows.Forms.Label();
+            this.pnlUsers = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlUsers.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbTitle
@@ -42,15 +50,76 @@ namespace RiskGame
             this.lbTitle.TabIndex = 0;
             this.lbTitle.Text = "Friends List";
             // 
+            // pnlUsers
+            // 
+            this.pnlUsers.Controls.Add(this.panel2);
+            this.pnlUsers.Location = new System.Drawing.Point(13, 50);
+            this.pnlUsers.Name = "pnlUsers";
+            this.pnlUsers.Size = new System.Drawing.Size(680, 327);
+            this.pnlUsers.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(64, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1NameGoesHere";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F);
+            this.button1.Location = new System.Drawing.Point(491, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Friend";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semilight", 12F);
+            this.label2.Location = new System.Drawing.Point(71, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Offline";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(6, 7);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(668, 64);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
             // frmFriendList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 389);
+            this.Controls.Add(this.pnlUsers);
             this.Controls.Add(this.lbTitle);
             this.Name = "frmFriendList";
             this.Text = "Risk: Friend List";
             this.Load += new System.EventHandler(this.frmFriendList_Load);
+            this.pnlUsers.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +128,11 @@ namespace RiskGame
         #endregion
 
         private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Panel pnlUsers;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- RiskGame/frmFriendList.cs
+++ RiskGame/frmFriendList.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Linq;
 
 namespace RiskGame
 {
     public partial class frmFriendList : Form
     {
+        int numberOfUsers;
+        string[,] users;
+        int currentLine;
+
         public frmFriendList()
         {
             InitializeComponent();
@@ -12,7 +19,63 @@ namespace RiskGame
 
         private void frmFriendList_Load(object sender, EventArgs e)
         {
+            currentLine = 0;
+            numberOfUsers = File.ReadAllLines("cachedUsers.conf").Count();
+            users = new string[numberOfUsers, 4];
+            string[] userDetails = File.ReadAllLines("cachedUsers.conf");
+            foreach (string user in userDetails)
+            {
+                var splitDetails = user.Split('~');
+                for (int i = 0; i < 2; i++)
+                {
+                    users[currentLine, i] = splitDetails[i];
+                }
+                AddToScreen(splitDetails[0], splitDetails[1]);
+                currentLine++;
+            }
+        }
 
+        private void AddToScreen(string username, string avatar)
+        {
+            // Add the user to the screen
+            Panel pnl = new Panel();
+            pnl.Name = "pnl_" + username;
+            int x = 6;
+            int y = 7;
+            y += (70*currentLine);
+            pnl.Location = new Point(x, y);
+            pnl.Size = new Size(668, 164);
+            Controls.Add(pnl);
+
+            PictureBox pb = new PictureBox();
+            pb.Name = "pb_" + username;
+            pb.Location = new Point(0, 0);
+            pb.Size = new Size(64, 64);
+            pb.ImageLocation = @avatar;
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pnl.Controls.Add(pb);
+
+            Label lb = new Label();
+            lb.Name = "lb_" + username;
+            lb.Text = username;
+            lb.Font = new Font("Segoe UI", 16);
+            lb.Location = new Point(70, 7);
+            pnl.Controls.Add(lb);
+
+            Label lbI = new Label();
+            lbI.Name = "lbI_" + username;
+            lbI.Text = username;
+            lbI.Font = new Font("Segoe UI", 12);
+            lbI.Location = new Point(70, 37);
+            pnl.Controls.Add(lbI);
+
+            Button btn = new Button();
+            btn.Name = "btn_" + username;
+            btn.Location = new Point(491, 7);
+            btn.Size = new Size(174, 48);
+            btn.Font = new Font("Segoe UI", 16);
+            btn.Text = "Add Friend";
+            pnl.Controls.Add(btn);
         }
     }
 }
