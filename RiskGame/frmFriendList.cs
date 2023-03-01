95cdf82ad19736025496695f3274aed7e9b8a468
jmatthewsc2k <jmatthews864@c2ken.net>
2023-03-01 10:39:38 +0000

    * Tried fixing friends list it didn't work.

--- RiskGame/frmFriendList.cs
+++ RiskGame/frmFriendList.cs
@@ -10,7 +10,7 @@ namespace RiskGame
     {
         int numberOfUsers;
         string[,] users;
-        int currentLine;
+        int currentLine = 0;
 
         public frmFriendList()
         {
@@ -19,17 +19,10 @@ namespace RiskGame
 
         private void frmFriendList_Load(object sender, EventArgs e)
         {
-            currentLine = 0;
-            numberOfUsers = File.ReadAllLines("cachedUsers.conf").Count();
-            users = new string[numberOfUsers, 4];
             string[] userDetails = File.ReadAllLines("cachedUsers.conf");
             foreach (string user in userDetails)
             {
                 var splitDetails = user.Split('~');
-                for (int i = 0; i < 2; i++)
-                {
-                    users[currentLine, i] = splitDetails[i];
-                }
                 AddToScreen(splitDetails[0], splitDetails[1]);
                 currentLine++;
             }
@@ -45,7 +38,7 @@ namespace RiskGame
             y += (70*currentLine);
             pnl.Location = new Point(x, y);
             pnl.Size = new Size(668, 164);
-            Controls.Add(pnl);
+            pnlUsers.Controls.Add(pnl);
 
             PictureBox pb = new PictureBox();
             pb.Name = "pb_" + username;
@@ -55,19 +48,22 @@ namespace RiskGame
             pb.SizeMode = PictureBoxSizeMode.Zoom;
             pnl.Controls.Add(pb);
 
+            Label lbI = new Label();
+            lbI.Name = "lbI_" + username;
+            lbI.Text = "Offline";
+            lbI.Font = new Font("Segoe UI", 10);
+            lbI.Location = new Point(70, 37);
+            pnl.Controls.Add(lbI);
+
             Label lb = new Label();
             lb.Name = "lb_" + username;
             lb.Text = username;
             lb.Font = new Font("Segoe UI", 16);
-            lb.Location = new Point(70, 7);
+            lb.AutoSize = false;
+            lb.Width = 200;
+            lb.Height = TextRenderer.MeasureText(lb.Text, lb.Font).Height;
+            lb.Location = new Point(70, 5);
             pnl.Controls.Add(lb);
-
-            Label lbI = new Label();
-            lbI.Name = "lbI_" + username;
-            lbI.Text = username;
-            lbI.Font = new Font("Segoe UI", 12);
-            lbI.Location = new Point(70, 37);
-            pnl.Controls.Add(lbI);
 
             Button btn = new Button();
             btn.Name = "btn_" + username;
